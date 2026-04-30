95e509a50409236ee97b3dda731af410d82b7847
labtechmovilfcitec-hash <Labtechmovil.fcitec@gmail.com>
2026-04-30 10:13:09 -0700

    feat: subir proyecto LABITO inicial

---
+++ Assets/Plugins/DollarsMoCap/Scripts/MoCapManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uOSC;
+using EVMC4U;
+
+namespace Dollars
+{
+    public class MoCapManager : MonoBehaviour
+    {
+        public int ListenOnPort = 39539;
+        uOscServer os;
+        ExternalReceiver re;
+        [SerializeField]
+        [Range(0.0f, 0.99f)]
+        private float _smoothness = 0.3f;
+        public float Smoothness
+        {
+            get { return _smoothness; }
+            set
+            {
+                _smoothness = value;
+                if (re != null)
+                {
+                    re.BoneFilter = value;
+                }
+            }
+        }
+        void OnValidate()
+        {
+            if (re != null)
+            {
+                re.BoneFilter = Smoothness;
+            }
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            os = gameObject.AddComponent<uOscServer>();
+            os.enabled = false;
+            os.port = ListenOnPort;
+            os.enabled = true;
+            re = gameObject.AddComponent<ExternalReceiver>();
+            re.BoneFilter = Smoothness;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
